12b936657816816a151f85dbecba096b233883c1
DESKTOP-MRDU3MM\81805 <nors-_-777@ezweb.ne.jp>
2023-11-09 20:05:08 +0900

    post.blade.phpをlaravel用の書き方に書き換え

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,7 @@
-<form method="post" action="">
- <h1>POST投稿画面</h1>
- <p>post<input type='text' name='text'></p>
- <input type='submit' value="投稿">
-
-</form>
-
+<h1>POST投稿画面</h1>
+{{ Form::open(['route' => ['twitter.index'], 'method' => 'post']) }}
+    @csrf
+    @method('post')
+    <p>post<textarea rows="4" cols="40"></textarea></p>
+{{ Form::submit('投稿', ['class' => 'btn btn-primary']) }}
+{{ Form::close() }}
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\TwitterController;
 Route::get('/', [UserController::class, 'create'])->name('user.create');
 Route::post('/created', [UserController::class, 'created'])->name('user.created');
 Route::get('/timeline', [TwitterController::class, 'index'])->name('twitter.index');
-Route::get('/tweet', [TwitterController::class, 'tweet'])->name('twitter.tweeet');
+Route::get('/tweet', [TwitterController::class, 'tweet'])->name('twitter.tweet');
 Route::post('/tweet', [TwitterController::class, 'tweet'])->name('twitter.tweet');
 
 
